7014f4874c67d4f5e9f17da4a296355dd87e16e6
等鱼 <2241563294@qq.com>
2022-10-12 14:06:38 +0800

    提交时间计算工具类

---
+++ TimeOperation.js
@@ -0,0 +1,105 @@
+export default class TimeOperation {
+  #date;
+  // 一小时
+  #ONEHOUR = 1000 * 60 * 60; // 毫秒
+  // 一天
+  #ONEDAY = 1000 * 60 * 60 * 24; // 毫秒
+  // 一分钟
+  #ONEMINUTES = 1000 * 60; // ms
+  constructor (date) {
+    this.#date = date ? new Date(date) : new Date();
+  }
+  // 获取一小时的ms
+  getOneHour() {
+    return this.#ONEHOUR;
+  }
+  // 获取一天的ms
+  getOneDay() {
+    return this.#ONEDAY;
+  }
+  // 获取单个值
+  getValue(type = 'yy') {
+    const typeObj = {
+      'yyyy': `${this.#date.getFullYear()}`,
+      'MM': `${this.#date.getMonth() + 1}`.padStart(2, '0'),
+      'dd': `${this.#date.getDate()}`.padStart(2, '0'),
+      'hh': `${this.#date.getHours()}`.padStart(2, '0'),
+      'mm': `${this.#date.getMinutes()}`.padStart(2, '0'),
+      'ss': `${this.#date.getSeconds()}`.padStart(2, '0'),
+      'y': this.#date.getFullYear(),
+      'M': this.#date.getMonth() + 1,
+      'd': this.#date.getDate(),
+      'h': this.#date.getHours(), 
+      'm': this.#date.getMinutes(),
+      's': this.#date.getSeconds()
+    };
+    return typeObj[type];
+  }
+  // 重新写入时间
+  setValue(date) {
+    this.#date = new Date(date);
+  }
+  // 调用自带函数
+  call(fnName, value) {
+    this.#date[`${fnName}`](value);
+  }
+  // 获取格式化的时间
+  format(type = 'yyyy-MM-dd hh:mm:ss') {
+    const types = type.split(/-+|:+| +|\.+|\//);
+    types.forEach(item => {
+      type = type.replace(item, this.get(item));
+    })
+    return type;
+  }
+  // 获取当天的 最早时间
+  getFirstTime() {
+    const date = new Date(this.#date);
+    date.setHours(0);
+    date.setMinutes(0);
+    date.setSeconds(0);
+    return new TimeOperation(date);
+  }
+  // 获取当天的 最后时间
+  getLastTime() {
+    const date = new Date(this.#date);
+    date.setHours(23);
+    date.setMinutes(59);
+    date.setSeconds(59);
+    return new TimeOperation(date);
+  }
+  // 获取当月份 第一天
+  getMonthFirstDay() {
+    const date = new Date(this.#date);
+    date.setDate(1);
+    return new TimeOperation(date);
+  }
+  // 获取当月份 最后一天
+  getMonthLastDay() {
+    const date = new Date(this.#date);
+    date.setMonth(this.#date.getMonth() + 1);
+    date.setDate(0);
+    return new TimeOperation(date);
+  }
+  // 获取n分钟之后
+  getAfterMinutes(minutes = 0) {
+    const date = new Date(this.#date);
+    date.setMinutes(date.getMinutes() + minutes);
+    return date;
+  }
+  // 获取n小时之后
+  getAfterHours(hours = 0) {
+    const date = new Date(this.#date);
+    date.setHours(date.getHours() + hours);
+    return date;
+  }
+  // 两个时间的差
+  timeDistance(endTime, type = 'd') {
+    const ms = new Date(endTime).getTime() - new Date(this.#date).getTime()
+    const timeObj = {
+      'd': this.#ONEDAY,
+      'h': this.#ONEHOUR,
+      'm': this.#ONEMINUTES
+    }
+    return Math.floor(ms / timeObj[type])
+  }
+}
